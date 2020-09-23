80e937af1469a378bc1ffffb84f480d707c66133
alexis600 <alexis.600@gmail.com>
2020-09-23 03:11:32 +0200

    link2y3

--- link2.py
+++ link2.py
@@ -1,17 +1,30 @@
 class Clase():
-    def __new__(cls):
-		print("xD")
-        print("new")
-   	    return super.__new__(cls)
+
+#	def __new__(cls, radio):
+#		#print("xD")
+#		print("new")
+#		return super().__new__(cls)
 	
-	def _init__(self):
+	def __init__(self, radio):
 		print("init")
-    
-	@classmethod
-	def saludar (cls, nombre):
+		self.radio = radio
+
+	@ classmethod 
+	def saludar(cls, nombre):
 		print("hola", nombre)
 		
-	##@staticmethod
 	
+	@property
+	def area(self):
+		return 3.1416*(self.radio**2)
+		
+		##@staticmethod
+
 
 Clase.saludar("Ale")
+
+
+circulo = Clase(10)
+print("el radio es:",circulo.radio)
+print("el área es:", str(circulo.area))
+			
