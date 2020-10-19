dd291021f7f222b79cc5b60327f7209da9d833d1
alexis600 <63431213+alexis600@users.noreply.github.com>
2020-10-19 15:11:03 -0300

    agrego conjuntos

--- Curso3_1.py
+++ Curso3_1.py
@@ -1,6 +1,6 @@
 print("\n" * 15)
 
-cadena = "Hola ALE   "
+cadena = "  Hola ALE   "
 
 
 reemplazada = cadena.replace("hola", "Chau").swapcase() # cambia mayusculas y minusculas
@@ -14,3 +14,45 @@ reemplazadaconRE = re.sub(patronMod, "Chau", cadena)
 print(cadena)
 print(reemplazada)
 print(reemplazadaconRE)
+
+#sacar espacios
+nueva = cadena.strip()
+print(nueva)
+lnueva = cadena.lstrip() #saca espacios a la izq y rstrip a la derecha
+print(lnueva)
+
+#cadena.upper() y cadena.lower() les cambia el case
+
+cadenaSplit = "hola ! chau ! mierda ! joder"
+#cadenaSplit.split(cadenaSplit('!'))
+
+print(cadenaSplit.split('!'))
+
+
+lista = [3,56,1,3,0,9,8,5]
+print(sorted(lista, reverse=True))
+print(sorted(lista))
+print(lista)
+
+#con lista.sort() ya queda guardada la original, antes la original no sufria cambios
+
+#tambien aplica a letras
+
+
+#CONJUNTOS
+conju1 = {'1', '2', '3', '4'}
+conju2 = set('456378')
+print(conju1)
+print(conju2)
+print(conju1, conju2, sep='\n')
+
+print(conju1 & conju2)
+print(conju1.intersection(conju2))
+
+print(conju1 | conju2)
+
+
+print(sorted(conju1 | conju2))
+
+
+print(sorted(conju2 | conju1))
