873a00493bdda52c448079954c467dd8c581c3dc
alexis600 <alexis600@gmail.com>
2020-10-19 22:56:16 -0300

    agrego leer csv

--- Curso3_2.py
+++ Curso3_2.py
@@ -11,12 +11,26 @@ moduloB.metodo()
 
 import csv
 
-doc = open("archivoW.csv", "w")
+doc = open("archivoW.csv", "a")
+# modo , "a" append
+# modo , "w" escribe, sobreescribe, crea
 
 doc_csv_w = csv.writer(doc)
 
-lista = ["Ale", 480]
+#lista = [["Ale", 300], ["Maxi", 500], ["Fiore", 600]]
+lista = [["Momo", 2]]
+#doc_csv_w.writerows(lista)
 
-#doc_csv_w.writerow(lista)
+#for x in lista:
+#    doc_csv_w.writerow(x)
+doc.close()
+
+#para solo leer , "r"
+
+doc = open("archivoW.csv", "r")
+doc_csv = csv.reader(doc)
+
+for nombre, nro in doc_csv:
+    print(nombre, nro)
 
 doc.close()
